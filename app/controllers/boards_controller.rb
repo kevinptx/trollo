51f69cbdf3648b7a10cba8cd9170d28a6762a9bc
Kevin Peery <kevin.peery@gmail.com>
2018-11-28 14:52:10 -0700

    update boards controller and views

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,49 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :edit, :update]
+
   def index
+    @boards = current_user.boards
   end
 
   def show
   end
 
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = current_user.boards.new(board_params)
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @board.update(board_params)
+      redirect_to boards_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @account.destroy
+    redirect_to boards_path
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name)
+  end
+
+  def set_board
+    @board = current_user.boards.find(params[:id])
   end
 end
